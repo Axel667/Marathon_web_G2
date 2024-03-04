c585c4ae34402193edc947bde5cbf1ca9291c099
Matis Breillad <mbreillad@gmail.com>
2024-03-04 14:28:53 +0100

    ajout du bon dataset csv

--- Code/ACP.R
+++ Code/ACP.R
@@ -1,7 +1,19 @@
-#ACP Départ de l'Aude
-
+                                                          #ACP Départ de l'Aude
+#Librairie
 library(FactoMineR)
 
+#Chemin du dossier
 setwd("C:/Users/mbrei/OneDrive/Bureau/Marathon du web/Marathon_web_G2")
 
-data <- read.csv2(file="enviroTab_pa_train.csv", dec='.')
+#Données complète
+data <- read.csv2(file="Data/Data.csv", dec='.',sep=',')
+summary(data)
+data
+
+#On enlève les deux premières colonnes qui décrivent juste la ville 
+data_acp <- data[,-c(1,2)]
+
+#On enlève les na
+
+
+PCA(data)
